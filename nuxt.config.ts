4cba17c5e6192fa9818c78f2390de2dba3995228
Theis Nielsen Haahr <Theishaahr@yahoo.dk>
2024-09-18 10:11:10 +0200

    Added vues and metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'My online store',
+      meta: [
+        {name : 'description', content: 'hobby project'}
+      ]
+    }
+  }
 })

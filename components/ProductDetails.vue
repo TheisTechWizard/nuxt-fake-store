334786b3682990e0fec1f6b710ed03b5ed76b8f9
Theis Nielsen Haahr <Theishaahr@yahoo.dk>
2024-09-20 15:11:05 +0200

    More styling

--- components/ProductDetails.vue
+++ components/ProductDetails.vue
@@ -1,19 +1,26 @@
 <template>
-    <div class="product-details">
-        <div class="image-content">
-            <img :src="product.image" alt="">
-        </div>
-        <div class="product-info">
-            <h3>{{ product.title }}</h3>
-            <p>Price: {{ product.price }}$</p>
-            <p>{{ product.description }}</p>
-            <p>{{ product.category }}</p>
+    <div class="product-details-wrapper">
+        <div class="product-details-card">
+            <div class="image-content">
+                <img :src="product.image" alt="">
+            </div>
+            <div class="product-info">
+                <h3>{{ product.title }}</h3>
+                <h4>USD {{ product.price }}$</h4>
+                <p>{{ product.description }}</p>
+                <p>Category: {{ product.category }}</p>
+                <button @click="handleClick">Add to cart</button>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup>
 const { product } = defineProps(['product'])
+
+function handleClick() {
+
+}
 </script>
 
 <style lang="scss"></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,16 @@
 <template>
     <header class="header-wrapper">
         <nav class="navigation-bar">
-            <h2>Fake store!</h2>
-            <ul>
-                <li><nuxt-link to="/products">Products</nuxt-link></li>
-            </ul>
-            <div class="shopping-cart">
-
+            <div class="nav-left-side">
+                <h2>Fake store!</h2>
+            </div>
+            <div class="nav-right-side">
+                <ul>
+                    <li><nuxt-link to="/products">Products</nuxt-link></li>
+                </ul>
+                <div class="shopping-cart">
+                    <img src="/assets/icons/shopping-bag.png" alt="">
+                </div>
             </div>
         </nav>
     </header>

01be2b490726da51fdf2fc9b144370598661e8eb
Theis Nielsen Haahr <Theishaahr@yahoo.dk>
2024-10-02 10:04:59 +0200

    Mobile burger menu in the making

---
+++ components/BurgerMenu.vue
@@ -0,0 +1,25 @@
+<template>
+    <transition name="burger-menu-slide">
+
+    </transition>
+    <div class="burger-menu-container" v-if="visible">
+        <button class="close-icon" @click="$emit('close')">X</button>
+        <ul class="menu">
+            <nuxt-link to="/products">Products</nuxt-link>
+            <nuxt-link v-for="category in uniqueCategories" :key="category"
+                :to="{ path: '/products', query: { category } }">
+                {{ category }}
+            </nuxt-link>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import { useProducts } from '@/composables/useProducts';
+const { uniqueCategories } = useProducts();
+
+const { visible } = defineProps(['visible'])
+
+</script>
+
+<style lang="scss" scoped></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,10 @@
     <header class="header-wrapper">
         <nav class="navigation-bar">
             <div class="nav-left-side">
-                <h2>Fake store!</h2>
+                <!-- <h2>Fake store!</h2> -->
+                <button class="burger-menu-icon" @click="toggleBurgerMenu">
+                    <img src="/assets/icons/burger-menu.png" alt="Burger menu">
+                </button>
             </div>
             <div class="nav-right-side">
                 <ul class="navigation-links">
@@ -12,8 +15,8 @@
                         {{ category }}
                     </nuxt-link> -->
                 </ul>
-                <button class="shopping-cart" @click="toggleSideBar">
-                    <img src="/assets/icons/shopping-bag.png" alt="">
+                <button class="shopping-cart" @click="toggleShoppingBar">
+                    <img src="/assets/icons/shopping-bag.png" alt="Shopping Cart">
                     <div class="cart-count" v-if="cart.length > 0">
                         <p>{{ totalCartItems }}</p>
                     </div>
@@ -22,8 +25,9 @@
         </nav>
     </header>
 
-    <ShoppingCartSideBar :visible="isSidebarVisible" @close="closeSidebar" />
-    <div v-if="isSidebarVisible" class="overlay"></div>
+    <ShoppingCartSideBar :visible="isShoppingBarVisible" @close="closeSidebar" />
+    <BurgerMenu :visible="isBurgerMenuVisible" @close="closeSidebar" />
+    <div v-if="isShoppingBarVisible || isBurgerMenuVisible" class="overlay"></div>
 
     <div>
         <slot />
@@ -41,16 +45,24 @@ import { useProducts } from '@/composables/useProducts';
 const { cart, totalCartItems } = useCart();
 const { uniqueCategories } = useProducts();
 
-const isSidebarVisible = ref(false);
+const isShoppingBarVisible = ref(false);
+const isBurgerMenuVisible = ref(false);
 
-const toggleSideBar = () => {
-    isSidebarVisible.value = !isSidebarVisible.value;
-    toggleBodyScroll(isSidebarVisible.value);
+
+const toggleShoppingBar = () => {
+    isShoppingBarVisible.value = !isShoppingBarVisible.value;
+    toggleBodyScroll(isShoppingBarVisible.value);
 }
 
 const closeSidebar = () => {
-    isSidebarVisible.value = false;
+    isShoppingBarVisible.value = false;
+    isBurgerMenuVisible.value = false;
     toggleBodyScroll(false);
+}
+
+const toggleBurgerMenu = () => {
+    isBurgerMenuVisible.value = !isBurgerMenuVisible.value;
+    toggleBodyScroll(isBurgerMenuVisible.value);
 }
 
 const toggleBodyScroll = (disableScroll) => {
